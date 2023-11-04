cd50fc92841cbd2fcefdd50bac005382c2d8e314
vichklu <119040501+vichklu@users.noreply.github.com>
2023-11-05 00:29:20 +0300

    find even index

---
+++ 006-FindEvenIndex/task.js
@@ -0,0 +1,19 @@
+export const FindEvenIndex = (arr) => {
+    const { length } = arr;
+    for (let i = 0; i < length; i++) {
+        let leftSum = 0;
+        let rightSum = 0;
+        for (let j = 0; j < i; j++) {
+            leftSum += arr[j];
+        }
+        for (let k = i + 1; k < length; k++) {
+            rightSum += arr[k];
+        }
+        if (leftSum === rightSum) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+console.log(`2 is even = ${FindEvenIndex([1, 2, 3, 4, 3, 2, 1])}`);
