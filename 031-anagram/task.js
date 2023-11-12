3fbed271eadd29fec3264d03c9ed3f596d319b8d
vichklu <119040501+vichklu@users.noreply.github.com>
2023-11-12 14:13:27 +0300

    anagram new

--- 031-anagram/task.js
+++ 031-anagram/task.js
@@ -1,9 +1,12 @@
+const sortString = (string) => string.split('').sort().join('');
+
 export const isAnagram = (test, original) => {
     test.toLowerCase();
     original.toLowerCase();
-    const sortedStr1 = test.split('').sort().join('');
-    const sortedStr2 = original.split('').sort().join('');
+    const sortedStr1 = sortString(test);
+    const sortedStr2 = sortString(original);
 
     return sortedStr1 === sortedStr2;
 };
+
 console.log(isAnagram('foefet', 'toffee'));
