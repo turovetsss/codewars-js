c8aa321e49c2630d7095660fabc18c13d6ca6177
vichklu <119040501+vichklu@users.noreply.github.com>
2023-11-22 01:50:31 +0300

    highest test

--- 047-highestRang/units.test.js
+++ 047-highestRang/units.test.js
@@ -1,8 +1,8 @@
 import { highestRank } from './task';
 
-test('', () => {
+test('should test', () => {
     expect(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12])).toStrictEqual(12);
 });
-test('', () => {
+test('should test2', () => {
     expect(highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10])).toStrictEqual(3);
 });
